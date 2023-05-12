44d83bcb32720c3ed782277aa5a9d848213e3220
Tari <ummu.fasa1886@gmail.com>
2023-05-12 15:04:14 +0700

    membuat halaman depan dan login

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/HalamanMuka/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        utama: ["Chicle"],
+      },
+    },
+  },
+  plugins: [],
+};
